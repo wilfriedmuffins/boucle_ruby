cc765472ad16531f78593dca0f2fa27888d72a9d
wilfriedmuffins <wilfried.moffen@live.fr>
2018-11-07 15:56:09 +0100

    expplication de la boucle pour afficher le contenu d'un tableau

---
+++ boucles.rb
@@ -0,0 +1,15 @@
+utilisateur = ["Alice", "Wilfried", "Adurey", "Dimitri"]
+
+
+#pour chaque élement du tableau utilisateur ranger cet utilisateur dans le viariable |utilisateur| le boucle sera éffectue selon le nombre d'élément du  tableau
+
+i=0
+utilisateur.each do |utilisateur|
+	puts i
+	i=i+1
+	puts utilisateur # affiche chaque utilisateur 
+end
+
+10.times do
+	puts "wilfried"
+end
