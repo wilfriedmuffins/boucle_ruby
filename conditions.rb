7b1e178a0fb6ecb2626575b8f5ee0d11e8ade97e
wilfriedmuffins <wilfried.moffen@live.fr>
2018-11-07 17:39:41 +0100

    exercution d'une boucle avec condition d'égalité

--- conditions.rb
+++ conditions.rb
@@ -1,4 +1,4 @@
-#boucle if en ruby
+#boucle if en ruby : condition inférieur ou supérieur
 a = 15
 
 puts "a est égal à : #{a}"
@@ -8,4 +8,24 @@ if a <= 10
 	puts "a est inférieur à 10"
 else
 	puts "a est supérieur à 10"
+end
+
+#condition avec tableau : condition d'égalité
+
+emails = ["wilfried.moffen@live.fr", "nom.prenom@gmail.com"]
+
+
+if emails.size == 1
+	puts "j'ai un email"
+else
+	puts "j'ai plus ou moin d'un email"
+end
+
+
+emails.each do |email|
+	if email == "wilfried.moffen@live.fr"
+		puts "Salut beau goss :)"
+	else email == "nom.prenom@gmail.com"
+		puts "je ne te connais pas"
+	end	
 end
