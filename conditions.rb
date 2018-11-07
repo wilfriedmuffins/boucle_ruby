c69ca166dc8c7bab1cdbb1a52a10bd20642749cb
wilfriedmuffins <wilfried.moffen@live.fr>
2018-11-07 18:13:45 +0100

    exercution de boucles avec condition logique, plusieurs conditions et conditions booleen

--- conditions.rb
+++ conditions.rb
@@ -29,3 +29,44 @@ emails.each do |email|
 		puts "je ne te connais pas"
 	end	
 end
+
+#conditon différent( no egal)
+
+b=12
+
+puts "a est égal à :"
+
+if b != 10
+	puts " c'est bon a est différent de 10"
+end
+
+#condtion ET logique(&&)
+
+if b !=10 && b!=12 
+	puts "b n'est à la fois différent de 10  et de 12"
+else 
+	puts "b n'est pas différents à la fois de 10 et de 12"
+end 
+
+#condition OU logique(||)
+
+if b!=11 || b!=20
+	puts " b n'est pas égeal à 11 ou 20"
+end
+
+#plusieurs conditions
+
+if (b!=3 || b!= 5) && b!="hello"
+	puts "b est différent de 3 ou 4 et différent de hello"
+end
+
+#conditon booléen
+
+c = 15
+
+mon_booleen = (c<23)
+puts mon_booleen
+
+if mon_booleen
+	puts "okey"
+end
